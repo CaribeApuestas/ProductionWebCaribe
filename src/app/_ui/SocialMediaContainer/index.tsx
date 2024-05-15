ea43b032e9e6bad4c95b4decae082c8cec79d459
JhonattanRom <jhonattanromdev@gmail.com>
2024-05-15 11:59:03 -0400

    feat: fix #1 SocialMediaContainer imgs component

--- src/app/_ui/SocialMediaContainer/index.tsx
+++ src/app/_ui/SocialMediaContainer/index.tsx
@@ -2,6 +2,11 @@ import styles from "./styles.module.css";
 import Image from "next/image";
 import Link from 'next/link'
 
+import CaribeLogo from '../../../../public/caribeApuestaLogo.svg'
+import CaribeWhatsapp from '../../../../public/icon/CaribeWhatsapp.svg'
+import CaribeInstagram from '../../../../public/icon/CaribeInstagram.svg'
+
+
 export const SocialMediaContainer = () => {
     let phoneNumber = '+58424-8236730';
     let message = 'Saludos CARIBEAPUESTA, ¿podrían brindarme su ayuda?'
@@ -9,7 +14,7 @@ export const SocialMediaContainer = () => {
         <div className={styles.SocialMediaContainer}>
             <div className={styles.Toggle_Title}>
                 <Image
-                    src={"/caribeApuestaLogo.svg"}
+                    src={CaribeLogo}
                     alt={"Caribe Apuesta Logo"}
                     width={50} height={50}
                 />
@@ -17,12 +22,12 @@ export const SocialMediaContainer = () => {
             </div>
             <div className={styles.mediaIcons}>
                 <a href={`https://api.whatsapp.com/send?phone=${phoneNumber}&text=${message}`} target="_blank">
-                    <Image className={styles.mediaIcon} src="/icon/CaribeWhatsapp.svg" alt="Burguer icon" width={60} height={60} />
+                    <Image className={styles.mediaIcon} src={CaribeWhatsapp} alt="whatsapp icon" width={60} height={60} />
                 </a>
                 <a href="https://www.instagram.com/caribeapuesta?igsh=bm5nbDZ6ODBybXB0" target="_blank">
-                    <Image className={styles.mediaIcon} src="/icon/CaribeInstagram.svg" alt="Burguer icon" width={60} height={60} />
+                    <Image className={styles.mediaIcon} src={CaribeInstagram} alt="Instagram icon" width={60} height={60} />
                 </a>
-                <Image className={styles.mediaIcon} src="/icon/CaribeYoutube.svg" alt="Burguer icon" width={60} height={60} />
+                {/*<Image className={styles.mediaIcon} src="/icon/CaribeYoutube.svg" alt="Burguer icon" width={60} height={60} />*/}
             </div>
         </div>
     )
