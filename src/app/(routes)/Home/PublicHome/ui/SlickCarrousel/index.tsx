5415757626ecd35bdcd946f1c891b63fd0dda39c
JhonattanRom <jhonattanromdev@gmail.com>
2024-05-15 11:48:12 -0400

    feat: fix #2 for SlickCarroules imgs component

--- src/app/(routes)/Home/PublicHome/ui/SlickCarrousel/index.tsx
+++ src/app/(routes)/Home/PublicHome/ui/SlickCarrousel/index.tsx
@@ -3,12 +3,12 @@ import styles from "./styles.module.css";
 import Slider from "react-slick";
 import Image from "next/image";
 import { useEffect, useState } from "react";
-import banner1 from '../../../../public/img/banner1.jpg';
-import banner2 from '../../../../public/img/banner2.jpg';
-import imgSliderHome1 from '../../../../public/img//imgSliderHome1.jpg';
-import imgSliderHome2 from '../../../../public/img//imgSliderHome2.jpg';
-import banner1Mobile from '../../../../public/img/banner1_800.jpg';
-import banner2Mobile from '../../../../public/img/banner2_800.jpg';
+import banner1 from '../../../../../../../public/img/banner1.jpg';
+import banner2 from '../../../../../../../public/img/banner2.jpg';
+import imgSliderHome1 from '../../../../../../../public/img/imgSliderHome1.jpg';
+import imgSliderHome2 from '../../../../../../../public/img/imgSliderHome2.jpg';
+import banner1Mobile from '../../../../../../../public/img/banner1_800.jpg';
+import banner2Mobile from '../../../../../../../public/img/banner2_800.jpg';
 
 const listForDesktop = [
   {
@@ -52,7 +52,7 @@ var settings = {
 
 export default function SlickCarrousel() {
   const [isMobile, setIsMobile] = useState<boolean>(false);
-  useEffect(()=> {
+  useEffect(() => {
     if (window.outerWidth >= 990) {
       setIsMobile(false);
     }
@@ -60,7 +60,7 @@ export default function SlickCarrousel() {
       setIsMobile(true);
     }
   }, []);
-  
+
   const renderImgList = (isMobile: boolean) => {
     const list = isMobile ? listForMobile : listForDesktop;
     return (
